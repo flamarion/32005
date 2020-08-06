d88951a6199339bdd21e228d018757f79bfa3364
Flamarion Jorge <flamarion@hashicorp.com>
2020-08-06 16:47:29 +0200

    Trying push the result of the script

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 
-
-# resource "null_resource" "git_commit_author" {
-#   provisioner "local-exec" {
-#     command = "echo {"git_commit_author":"$(git log -1 | grep ^Author | cut -f 2 -d : | sed 's/^ //g')"}"
-#   }
-# }
-
 data "external" "git_commit_author" {
   program     = ["${path.module}/git_commit_author.sh"]
   #working_dir = path.root
 }
 
-output "git_commit_author" {
-  value = data.external.git_commit_author.result
+
+resource "null_resource" "git_commit_author" {
+  provisioner "local-exec" {
+    command = "echo ${data.external.git_commit_author.result}"
+  }
 }
+
+# output "git_commit_author" {
+#   value = data.external.git_commit_author.result
+# }
