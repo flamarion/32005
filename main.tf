9212c9cbfb4a34cf3089cc69c4e3fa3d35eccb53
Flamarion Jorge <flamarion@hashicorp.com>
2020-08-06 16:18:06 +0200

    Testing again

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
 data "external" "git_commit_author" {
-  program     = ["/bin/bash", "-c", "echo \"{\\\"git_commit_author\\\":\\\"$(git log -1 | grep ^Author | cut -f 2 -d : | sed 's/^ //g')\\\"}\""]
+  program     = ["/bin/bash", "-c", "echo {\"git_commit_author\":\"$(git log -1 | grep ^Author | cut -f 2 -d : | sed 's/^ //g')\"}"]
   working_dir = path.root
 }
 
+# resource "null_resource" "git_commit_author" {
+#   provisioner "local-exec" {
+#     command = "echo {"git_commit_author":"$(git log -1 | grep ^Author | cut -f 2 -d : | sed 's/^ //g')"}"
+#   }
+# }
 
 output "git_commit_author" {
     value = data.git_commit_author.result
