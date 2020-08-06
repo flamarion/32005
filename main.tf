6b0d54310a15069b87838c16f1388d7ec592d823
Flamarion Jorge <flamarion@hashicorp.com>
2020-08-06 17:00:20 +0200

    Protecting with single quotes

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 
 data "external" "git_commit_author" {
   program     = ["${path.module}/git_commit_author.sh"]
-  #working_dir = path.root
+  working_dir = path.module
 }
 
 
 resource "null_resource" "git_commit_author" {
   provisioner "local-exec" {
-    command = "echo ${data.external.git_commit_author.result["git_commit_author"]}"
+    command = "echo '${data.external.git_commit_author.result["git_commit_author"]}'"
   }
 }
 
